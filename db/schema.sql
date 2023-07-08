7a9228683563754545387ec89f10a5c0b4e3a0e8
michellesambuu <michelle.sambuu@gmail.com>
2023-07-08 17:42:52 -0500

    updated file

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,82 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db;
+const inquirer = require("inquirer")
+const db = require("./db/connection")
+require("console.table")
 
-CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY ,
-    
-    name VARCHAR(30)
-);
-CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY ,
-    title  VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
-);
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY ,
-    first_name  VARCHAR(30),
-    last_name  VARCHAR(30),
-    role_id INT,
-    FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
-  manager_id INT,
-     FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
-);
+function mainPrompts() {
+    inquirer.prompt([{
+        type: "list",
+        name: "choose",
+        message: "which action would you like to take?",
+        choices: ["View all employees", "View all departments", "View all roles", "Add employee", "Add department", "Add role", "Update employee role", "quit"]
+
+    }])
+        .then((answer) => {
+            switch (answer.choose) {
+                case "View all employees":
+                    viewAllEmployees()
+                    break;
+                case "View all departments":
+                    viewAllDepartments()
+                    break;
+                case "View all roles":
+                    viewAllRoles()
+                    break;
+                case "Add employee":
+                    addEmployee()
+                    break;
+                case "Add department":
+                    addDepartment()
+                    break;
+                case "Add role":
+                    addRole()
+                    break;
+                case "Update employee role":
+                    updateEmployeeRole()
+                    break;
+                default:
+                    quit()
+
+
+            }
+        })
+}
+
+function viewAllEmployees() {
+    db.query(`SELECT * FROM employee LEFT JOIN role ON employee.role_id=role.id`, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        mainPrompts()
+    })
+}
+function viewAllDepartments() {
+    db.query(`SELECT * FROM department`, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        mainPrompts()
+    })
+}
+function viewAllRoles() {
+    db.query(`SELECT * FROM role LEFT JOIN department ON role.department_id=department.id `, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        mainPrompts()
+    })
+}
+
+
+function addDepartment() {
+    inquirer.prompt([{
+        type: "input",
+        name: "deptName",
+        message: "What is the name of the department you would like to add?"
+    }])
+        .then((answer) => {
+            db.query(`INSERT INTO department SET ?`, { name: answer.deptName }, (err, res) => {
+                if (err) throw err
+                console.log("Department added");
+                mainPrompts()
+            })
+        })
+}
+
+mainPrompts()
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,33 @@
-INSERT INTO department (id,name)
-VALUES (1,"Finance"),
-       ( 2,"Accounting"),
-       (3, "Marketing"),
-       (4,"Engineering");
+use employees;
 
-INSERT INTO role (id,title,salary,department_id)
-VALUES (1,"Financial analyst",63.000, 1),
-       ( 2,"Accountant", 45.000, 2),
-       (3, "Marketing analyst",40.000,3),
-       (4,"Software Engineer", 50.000,4);
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-INSERT INTO employee (id,first_name,last_name,role_id,manager_id)
-VALUES (1,"Max","Pat", 1,null),
-       ( 2,"Mary","Roe",  2,1),
-       (3, "Edward","Kane",3,1),
-       (4,"John", "Reid",4, null);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
